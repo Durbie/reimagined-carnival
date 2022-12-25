7e1f71f725983ff5f6aa8b2bf6bf492977c2baa8
Durbie <gavinwappler@gmail.com>
2022-12-25 09:48:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -56,13 +56,21 @@ function readCookie(name) {
       var c = ca[i];
       while (c.charAt(0)==' ') c = c.substring(1,c.length);
       if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-      else{
-        alert("WORK");
-      }
   }
   return null;
   }
   
+
+  function readCookie2(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 1) return c.substring(nameEQ.length,c.length);
+    }
+    alert("WORJ");
+    }
   
   window.onload = function () { document.getElementById("options").selectedIndex = readCookie("myDDIdx"); } 
 
