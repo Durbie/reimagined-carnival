460da4e7c14eb44a917997b0d5d9a33724b807f1
Durbie <gavinwappler@gmail.com>
2022-12-25 09:50:55 -0500

    WORK DAM YU

--- script.js
+++ script.js
@@ -67,10 +67,9 @@ function readCookie(name) {
     for(var i=0;i < ca.length;i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 1) return c.substring(nameEQ.length,c.length);
+        if (c.indexOf(nameEQ) == 1)  alert("WORJ");
     }
-    alert("WORJ");
-    }
+  }
   
   window.onload = function () { document.getElementById("options").selectedIndex = readCookie("myDDIdx"); } 
 
