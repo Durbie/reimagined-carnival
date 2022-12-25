e0100cd1279bc74a31a08d6c1483e89523c51474
Durbie <gavinwappler@gmail.com>
2022-12-25 10:14:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,20 +33,18 @@ if (window.sidebar) {
 }
 
 // options cookie
-
-
-function readCookie(name) {
+function readCookie(name, value) {
   if (value === 1) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    alert("omg this might work");
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+    alert("The value is 1!");
   }
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
 }
 
 
